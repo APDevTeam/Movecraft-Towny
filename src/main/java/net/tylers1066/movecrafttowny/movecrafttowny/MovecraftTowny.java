bc8b3219b65617d8d2342eabb4c546ece205c844
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-03-04 14:46:07 -0600

    Initial commit

---
+++ src/main/java/net/tylers1066/movecrafttowny/movecrafttowny/MovecraftTowny.java
@@ -0,0 +1,16 @@
+package net.tylers1066.movecrafttowny.movecrafttowny;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MovecraftTowny extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
